c8c748bf92ff44e806a9a63d15bf14f9da8f93d2
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-09-19 10:29:45 -0300

    tetse

--- api/estudantes.js
+++ api/estudantes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/estudantes',(req,res)=>{
-    res.send('lista de estudantes');
+    res.send('lista de estudantes')
 })
 
 
--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const routers = require('./api');
 const app = express();
 
 app.use(express.json());
+app.use('/',routers);
 
 // criando o servidor 
 app.listen(3000,()=>{
