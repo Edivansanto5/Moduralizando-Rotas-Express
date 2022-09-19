4583f6135025a860d9d8c6f1b618de599e703724
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-09-19 09:32:23 -0300

    criando o tratamentos para /

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
 // importando o express para ser usado em cada modula
+const { application, request } = require('express');
 const express = require('express');
 const router = express.Router();
+
+// fazendo os tratamentos da rotas de barras
+app.get('/',(req,res)=>{
+    res.send('app online ')
+});
