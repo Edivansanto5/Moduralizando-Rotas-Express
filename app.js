72efa7334a244e07bd1a7c69256a5b02433292eb
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-09-19 09:34:08 -0300

    removendo a parte de tratamento da barra do arquivo app

--- app.js
+++ app.js
@@ -2,11 +2,6 @@
 const app = express();
 app.use(express.json());
 
-// criando a rota da barra
-app.get('/',(req,res) => {
-    res.send('App Online');
-});
-
 // criando a rota de curso
 app.get('/cursos',(req,res) => {
     res.send('Lista de cursos');
