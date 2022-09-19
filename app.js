1e666d0ce4622709076898a938bf218113a25280
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-09-19 09:06:40 -0300

    criando a rota da barra

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const { sendfile } = require("express/lib/response");
+
 const app = express();
 app.use(express.json());
 
+// criando a rota da barra
+app.get('/',(req,res) => {
+    res.send('App Online')
+})
